d0cf785782d3c9bf631f309635c69c7014ec9fc6
Sysca11 <sysca11@github.com>
2020-04-06 10:56:54 +0800

    do not depend on vcpkg anylonger. fix missing chakra.vcxproj

--- headers/stl/viewhelper.h
+++ headers/stl/viewhelper.h
@@ -2,7 +2,7 @@
 #include<string_view>
 using std::string_view;
 template <typename T, typename std::enable_if_t<!std::is_pointer_v<T>, int> = 0>
-inline static string_view to_view(T& x) {
+inline static constexpr string_view to_view(T& x) {
     return string_view((const char*)&x, sizeof(T));
 }
 template<typename T=int>
@@ -12,10 +12,17 @@ static constexpr T atoi(std::string_view sv) {
     const char* c = sv.data();
     auto sz = sv.size();
     for (decltype(sz) i = 0; i < sz; ++i) {
-        if (c[i] == '-') { fg = 1; }
+        if constexpr (!std::is_unsigned_v<T>) {
+            if (c[i] == '-') { fg = 1; }
+        }
         if (!(c[i] >= '0' && c[i] <= '9')) continue;
         res *= 10;
         res += c[i] - '0';
     }
-    return fg ? -res : res;
+    if constexpr (!std::is_unsigned_v<T>) {
+        return fg ? -res : res;
+    }
+    else {
+        return res;
+    }
 }
